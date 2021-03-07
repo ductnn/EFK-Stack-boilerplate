736510065af7eaa22f3432d8a70be0678c28ead0
ductnn <trannhuduc531998@gmail.com>
2021-03-07 18:47:58 +0700

    Add file bash auto deploy

---
+++ run.sh
@@ -0,0 +1,12 @@
+# ES
+# kubectl create -f kube-logging.yaml
+# kubectl create -f elastic-service.yaml
+# kubectl create -f elastic-statefulset.yaml
+# kubectl create -f kibana.yml
+
+## Fluent bit
+kubectl create -f fluent-bit-service-account.yml
+kubectl create -f fluent-bit-role.yml
+kubectl create -f fluent-bit-role-binding.yml
+kubectl create -f fluent-bit-configmap.yml
+kubectl create -f fluent-bit-ds-minikube.yml
