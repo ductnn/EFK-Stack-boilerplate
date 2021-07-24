f55476661d3ce71123e97b37a26af4716e8bee07
ductnn <trannhuduc531998@gmail.com>
2021-07-24 23:31:52 +0700

    Update

--- run.sh
+++ run.sh
@@ -1,15 +1,19 @@
 set -a
 source .env
 
-# ES
-# kubectl create -f kube-logging.yaml
-# kubectl create -f elastic-service.yaml
-# kubectl create -f elastic-statefulset.yaml
-# kubectl create -f kibana.yml
+## Common namespace
+# envsubst < ./kube-logging.yaml | kubectl create -f -
+
+## ES
+# envsubst < ./elastic-service.yaml | kubectl create -f -
+# envsubst < ./elastic-statefulset.yaml | kubectl create -f -
+
+## Kibana
+# envsubst < ./kibana.yml | kubectl create -f -
 
 ## Fluent bit
-kubectl create -f fluent-bit-service-account.yml
-kubectl create -f fluent-bit-role.yml
-kubectl create -f fluent-bit-role-binding.yml
-kubectl create -f fluent-bit-configmap.yml
-kubectl create -f fluent-bit-ds-minikube.yml
+envsubst < ./fluent-bit-service-account.yml | kubectl create -f -
+envsubst < ./fluent-bit-role.yml | kubectl create -f -
+envsubst < ./fluent-bit-role-binding.yml | kubectl create -f -
+envsubst < ./fluent-bit-configmap.yml | kubectl create -f -
+envsubst < ./fluent-bit-ds-minikube.yml | kubectl create -f -
